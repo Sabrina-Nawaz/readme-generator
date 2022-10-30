93f7ef8585634da57d7543623991dd295cb876ce
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-10-30 16:46:53 -0400

    Installed packages and created questions for inquirer prompts

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+//const { input } = require('answers');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt
+const questions = ([
+    {
+        //Project title
+        type: "input",
+        message: "What is your project named?",
+        name: "name",
+    },
+    {
+        //Enter description of your project
+        type: "input",
+        message: "How would you describe your project?",
+        name: "description",
+    },
+    {
+        //Enter installation instructions
+        type: "input",
+        message: "How do you install your project?",
+        name: "installation",
+    },
+    {
+        //Enter usage information 
+        type: "input",
+        message: "How do you use your project?",
+        name: "usage",
+    },
+    {
+        //Contribution guidelines 
+        type: "input",
+        message: "Are there any contribution guidelines?",
+        name: "contribution",
+    },
+    {
+        //Test instructions 
+        type: "input",
+        message: "How do you test your project?",
+        name: "test",
+    },
+]
+);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
