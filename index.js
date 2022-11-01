efb9d5c22ccdf4937e5b650e7e96a2af51a64c28
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-11-01 17:22:32 -0400

    Deleted test md documents and created licenses function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-
+//// TODO: Create an array of questions for user input
 inquirer 
 .prompt
 ([
@@ -10,80 +10,97 @@ inquirer
         //Project title
         type: "input",
         message: "What is your project named?",
-        name: "Title",
+        name: "title",
     },
     {
         //Enter description of your project
         type: "input",
         message: "How would you describe your project?",
-        name: "Description",
+        name: "description",
     },
     {
         //Enter installation instructions
         type: "input",
         message: "How do you install your project?",
-        name: "Installation",
+        name: "installation",
     },
     {
         //Enter usage information 
         type: "input",
         message: "How do you use your project?",
-        name: "Usage",
+        name: "usage",
     },
     {
         //Contribution guidelines 
         type: "input",
         message: "Are there any contribution guidelines?",
-        name: "Contribution",
+        name: "contribution",
     },
     {
         //Test instructions 
         type: "input",
         message: "How do you test your project?",
-        name: "Tests",
+        name: "tests",
     },
     {
         //Create a list of licenses  
         type: "list",
         message: "Which license was used for this project?",
-        name: "License",
+        name: "license",
         choices: [
             "GPL", 
             "Mozilla Public License", 
-            "MIT", 
-            "GNU", 
-            "Apache", 
+            "MIT",  
+            "Apache",
+            "WTFPL", 
             "N/A"],
     },
     {
         //Enter Github username  
         type: "input",
         message: "Enter your Github username",
-        name: "GitHub",
-    },
-    {
-        //Enter Github username  
-        type: "input",
-        message: "Enter your Github username",
-        name: "GitHub",
+        name: "github",
     },
     {
         //Enter Email  
         type: "input",
         message: "Enter your GitHub email",
-        name: "Email",
+        name: "email",
     },
     {
         //Enter GitHub URL  
         type: "input",
         message: "Enter your GitHub repo URL",
-        name: "GitHub",
+        name: "repo",
     },
 
-])
+]
+).then(({
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    tests,
+    license,
+    github,
+    email,
+    repo
+}) => {
+// layout for README
+const layout = `# ${title}`
+writeToFile(title, layout)
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) { }
+}
+)
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, content) {
+    const name = fileName.toLowerCase().split(' ').join('_')
+    fs.writeFile(`./${name}.md`, content, () => {
+        console.log("hi")
+    })
+ }
 
 // // TODO: Create a function to initialize app
 // function init() { }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  const licenses = {
+      "GPL": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", 
+      "Mozilla Public License": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+      "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT",  
+      "Apache": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      "WTFPL": "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)", 
+      "N/A": ''
+  }
+  return licenses[license]
+}
+renderLicenseBadge(license)
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
